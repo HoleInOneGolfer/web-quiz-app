bf7fa99b76246a7ab4dc84049299a5522a0a6e58
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2025-08-03 22:37:02 -0500

    fix __init__

--- __init__.py
+++ __init__.py
@@ -1,1 +1,3 @@
 from .data_manager import pd, load_excel_data, extract_unique_values_and_clean, get_quiz_data_dict, get_quiz_data_dataframe, get_quiz_data_json, df_to_html, df_to_dict
+
+from .__main__ import app
