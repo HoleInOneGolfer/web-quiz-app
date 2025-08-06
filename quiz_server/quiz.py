87937af68ef569c0c35a890f3d8b7827785af237
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2025-08-06 01:25:32 -0500

    yes

--- quiz_server/quiz.py
+++ quiz_server/quiz.py
@@ -4,17 +4,26 @@ import os
 
 bp = Blueprint('quiz', __name__)
 
+def load_data(file_path):
+    if os.path.exists(file_path):
+        return pd.read_excel(file_path)
+    else:
+        df = pd.DataFrame()
+        df.to_excel(file_path, index=False)
+        return df
+
+
 # === Endpoints === #
 
 @bp.route('/', methods=['GET'])
 def list_quizzes():
-    quiz_data = pd.read_excel(current_app.config['QUIZ_EXCEL_FILE'])
+    quiz_data = load_data(current_app.config['QUIZ_EXCEL_FILE'])
     quiz_list = pd.unique(quiz_data['quiz_name']).tolist()
     return render_template('list.jinja', title="Quiz List", quiz_list=quiz_list)
 
 @bp.route('/quiz/<quiz_name>', methods=['GET'])
 def get_quiz(quiz_name):
-    quiz_data = pd.read_excel(current_app.config['QUIZ_EXCEL_FILE'])
+    quiz_data = load_data(current_app.config['QUIZ_EXCEL_FILE'])
     quiz_data = quiz_data[quiz_data['quiz_name'] == quiz_name]
     quiz_data = quiz_data.to_dict(orient='records')
     return  render_template('quiz.jinja', title=quiz_name, quiz_name=quiz_name, quiz_data=quiz_data)
@@ -25,7 +34,7 @@ def submit_session():
     session_data = pd.DataFrame([session_data])
     session_data.columns = session_data.columns.str.lower()
 
-    results = pd.read_excel(current_app.config['RESULTS_EXCEL_FILE'])
+    results = load_data(current_app.config['RESULTS_EXCEL_FILE'])
 
     results = pd.concat([results, session_data], ignore_index=True)
     results.to_excel(current_app.config['RESULTS_EXCEL_FILE'], index=False)
@@ -35,12 +44,12 @@ def submit_session():
 
 @bp.route('/quiz_results', methods=['GET'])
 def get_results():
-    results = pd.read_excel(current_app.config['RESULTS_EXCEL_FILE'])
+    results = load_data(current_app.config['RESULTS_EXCEL_FILE'])
     results_html = results.to_html(header="true", index=False, na_rep='')
     return render_template('data.jinja', title='Quiz Results', data_html=results_html)
 
 @bp.route('/quiz_data', methods=['GET'])
 def get_master_list():
-    quiz_data = pd.read_excel(current_app.config['QUIZ_EXCEL_FILE'])
+    quiz_data = load_data(current_app.config['QUIZ_EXCEL_FILE'])
     quiz_data_html = quiz_data.to_html(header="true", index=False, na_rep='')
     return render_template('data.jinja', title='Quiz Data', data_html=quiz_data_html)
