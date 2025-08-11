56c323b028205fcbba97dc066f2045797761eb42
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2025-08-10 19:38:18 -0500

    project

--- quiz_server/quiz.py
+++ quiz_server/quiz.py
@@ -1,12 +1,27 @@
-from flask import Blueprint, current_app, redirect, render_template, request, send_from_directory
+from flask import Blueprint, current_app, render_template, request, send_from_directory
 import pandas as pd
-import os
-
-from .data import load_data, save_data
 
 bp = Blueprint('quiz', __name__)
 
-# === Endpoints === #
+def load_data(file_path):
+    try:
+        return pd.read_csv(file_path)
+    except FileNotFoundError:
+        return pd.DataFrame()
+    except pd.errors.EmptyDataError:
+        return pd.DataFrame()
+
+def save_data(df, file_path):
+    df.columns = df.columns.str.lower()
+
+    file_df = load_data(file_path)
+
+    if file_df.empty:
+        file_df = pd.DataFrame(columns=df.columns)
+
+    file_df = pd.concat([file_df, df], ignore_index=True)
+    file_df.to_csv(file_path, index=False)
+
 @bp.route('/data/<path:filename>', methods=['GET'])
 def data(filename):
     return send_from_directory(current_app.config['DATA_DIR'], filename)
