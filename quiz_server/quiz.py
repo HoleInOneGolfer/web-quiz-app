1c26ecee8b4020020d1804467668191360acef80
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2025-08-17 13:29:32 -0500

    remove utils

--- quiz_server/quiz.py
+++ quiz_server/quiz.py
@@ -1,8 +1,27 @@
 from flask import Blueprint, current_app, render_template, request, send_from_directory
-from .utils import load_csv, save_csv
 import pandas as pd
 
 bp = Blueprint('quiz', __name__)
+
+def load_csv(file_path):
+    try:
+        return pd.read_csv(file_path)
+    except FileNotFoundError:
+        return pd.DataFrame()
+    except pd.errors.EmptyDataError:
+        return pd.DataFrame()
+
+def save_csv(df, file_path):
+    df.columns = df.columns.str.lower()
+
+    file_df = load_csv(file_path)
+
+    if file_df.empty:
+        file_df = pd.DataFrame(columns=df.columns)
+
+    file_df = pd.concat([file_df, df], ignore_index=True)
+    file_df.to_csv(file_path, index=False)
+
 
 @bp.route('/data/<path:filename>', methods=['GET'])
 def data(filename):
